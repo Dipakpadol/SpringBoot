731b768032a0a07ff82e699e566592f812beef9d
Dipak Padol <dipak.padol@maiara.in>
2024-08-20 18:13:28 +0530

    Added product controller

---
+++ src/main/java/com/firstwebapp/controller/ProductContoller.java
@@ -0,0 +1,23 @@
+package com.firstwebapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstwebapp.dto.Product;
+import com.firstwebapp.service.ProductService;
+
+@RestController
+public class ProductContoller {
+
+	@Autowired
+	ProductService productService;
+
+	@GetMapping("/productDetails")
+	private List<Product> getProducts() {
+		List<Product> productLists = productService.getProducts();
+		return productLists;
+	}
+}
